12483f96a0f09a750c7b6f28d70b44b9f90f1a5c
musashi ueda <ueda@artisan.tips>
2018-11-16 23:01:49 +0900

    changed player status.

--- Assets/Project/Scripts/Scenes/Battle/Logic/Parameter/Parameter.cs
+++ Assets/Project/Scripts/Scenes/Battle/Logic/Parameter/Parameter.cs
@@ -722,10 +722,10 @@ namespace BattleLogic {
             int coefficient = maxLevel / 20; //ratiryData.coefficient;
             // ベースパラメータの計算
             BaseMaxHp = CalcParameter (cardData.initial_hp, cardData.max_hp, _level, maxLevel, coefficient, cardData.hp_growth_type) + Weapon.Hp + _magikite.Select(x => x.Hp).Sum() + growthBoardHp;
-            BaseAttack = CalcParameter (cardData.initial_attack, cardData.max_attack, _level, maxLevel, coefficient, cardData.attack_growth_type) + Weapon.Attack + _magikite.Select(x => x.Attack).Sum() + growthBoardAtk;
-            BaseDefense = CalcParameter (cardData.initial_defence, cardData.max_defence, _level, maxLevel, coefficient, cardData.defence_growth_type) + Weapon.Defense + _magikite.Select(x => x.Defense).Sum() + growthBoardDef;
-            BaseAgility = CalcParameter (cardData.initial_agility, cardData.max_agility, _level, maxLevel, coefficient, cardData.agility_growth_type) + Weapon.Agility + _magikite.Select(x => x.Agility).Sum() + growthBoardAgi;
-            BaseLuck = cardData.luck;
+			BaseAttack = 99999999;//CalcParameter (cardData.initial_attack, cardData.max_attack, _level, maxLevel, coefficient, cardData.attack_growth_type) + Weapon.Attack + _magikite.Select(x => x.Attack).Sum() + growthBoardAtk;
+			BaseDefense = 99999999;//CalcParameter (cardData.initial_defence, cardData.max_defence, _level, maxLevel, coefficient, cardData.defence_growth_type) + Weapon.Defense + _magikite.Select(x => x.Defense).Sum() + growthBoardDef;
+			BaseAgility = 99999999;//CalcParameter (cardData.initial_agility, cardData.max_agility, _level, maxLevel, coefficient, cardData.agility_growth_type) + Weapon.Agility + _magikite.Select(x => x.Agility).Sum() + growthBoardAgi;
+			BaseLuck = 99999999;//cardData.luck;
 
             // TODO: あとで実際の取得情報から生成するように修正する
             var skillList = MasterDataTable.card_acquire_skill.DataList.
@@ -766,7 +766,7 @@ namespace BattleLogic {
             RecalcParameterVariation ();
             Hp = MaxHp;
             _MaxSp = 100;
-            Sp = 0;
+            Sp = 100;
 
             _dropItems = null;
 
